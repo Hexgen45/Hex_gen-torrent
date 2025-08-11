5da171785f3062087c8e14c5f86e92bc17ab5ca6
Obaid <obaidahmedkhan421@gmail.com>
2025-08-11 21:15:57 +0500

    Add functions declrations and response type checking logic

--- tracker.js
+++ tracker.js
@@ -1,15 +1,56 @@
+"use strict";
 
 import dgram from 'dgram'
 import urlParse from 'url'
 
 export function getPeers (torrent, callback) {
     const socket = dgram.createSocket('udp4')
-    const url = urlParse.parse(torrent["announce"])
+    const url = torrent["announce"]
 
-    
+    udpSend(socket, buildConnReq(), url);
+
+    socket.on('message', (response)=>{
+        if (respType(response) === 'connect'){
+
+            const connResp = parseConnResp(response)
+
+            const announceReq = buildAnnounceReq(connResp.connectionId);
+
+            udpSend(socket, announceReq, url);
+        }
+        else if (respType(response) === 'announce') {
+            const announceResp = parseAnnounceResp(response);
+
+            callback(announceResp.peers)
+        }
+    });
+
+};
+
+function udpSend(socket, message, rawUrl, callback=()=>{}) {
+    const url = urlParse.parse(rawUrl);
+    socket.send(message, 0, message.length, url.port, url.host, callback);
 
 }
 
 
 
-get
+function respType(resp){
+
+}
+
+function buildConnReq(){
+
+}
+
+function parseConnResp(resp){
+
+}
+
+function buildAnnounceReq(connId){
+
+}
+
+function parseAnnounceResp(resp){
+
+}
