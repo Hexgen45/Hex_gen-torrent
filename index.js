3425271f524f411534cd0ba4a5f86775d0d735dc
Obaid <obaidahmedkhan421@gmail.com>
2025-08-11 17:10:37 +0500

    Open tracker, get tracker url

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 'use strict';
-const fs = require('fs')
-const torrent = fs.readFileSync('./puppy.torrent')
-console.log(torrent.toString('utf-8'))
+
+import fs from 'fs'
+import bencode from 'bencode'
+
+import dgram from 'dgram'
+import urlParser from 'url'
+
+const torrent = bencode.decode(fs.readFileSync('./puppy.torrent'),'utf-8');
+
+const url = urlParser.parse(torrent["announce"])
+
+const socket = dgram.createSocket('udp4')
+
+const msg1 = Buffer.from('Hello?', 'utf-8')
+
+socket.send(msg1, 0, msg1.length, url.port, url.host, ()=>{})
+
+socket.on('message', (msg)=>{
+    console.log("message is", msg)
+})
