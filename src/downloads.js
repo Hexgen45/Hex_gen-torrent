77637f904ee0e43e54b258ea11b42a8a5fa0fcf2
Obaid <obaidahmedkhan421@gmail.com>
2025-08-27 13:32:18 +0500

    add message.js

--- src/downloads.js
+++ src/downloads.js
@@ -1,6 +1,20 @@
 'use strict';
 import { getPeers } from './src/tracker.js';
+import net from 'net'
 
 getPeers(torrent, peers => {
-    console.log('list of peers', peers)
+    peers.foreach();
 })
+
+function Download(peer){
+    const socket = net.Socket();
+    socket.on('error', console.log);
+    socket.connect(peer.port, peer.ip, ()=>{
+        //socket.write(....)
+    });
+    
+    socket.on('data', data =>{
+        //handle response here
+    });
+}
+
