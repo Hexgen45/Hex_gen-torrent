4797d37b25706a449e3fc9b07b3729533ecc2f58
Obaid <obaidahmedkhan421@gmail.com>
2025-08-27 21:03:42 +0500

    add onWholeMsg function

--- src/downloads.js
+++ src/downloads.js
@@ -12,9 +12,28 @@ function Download(peer){
     socket.connect(peer.port, peer.ip, ()=>{
         //socket.write(....)
     });
+
+    
     
     socket.on('data', data =>{
         //handle response here
     });
 }
 
+
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
+}
