80df4cbbf1a894144a88e277074c8d11c7fd095d
Obaid <obaidahmedkhan421@gmail.com>
2025-08-28 12:32:03 +0500

    add non-handshake message handlers

--- src/downloads.js
+++ src/downloads.js
@@ -1,6 +1,6 @@
 'use strict';
 import { getPeers } from './src/tracker.js';
-import {buildHandshake} from './message.js'
+import {buildHandshake, msg_parse} from './message.js'
 import net from 'net'
 
 getPeers(torrent, peers => {
@@ -37,7 +37,18 @@ function onWholeMsg(socket, callback) {
 }
 
 function msgHandler(msg, socket) {
-  if (isHandshake(msg)) socket.write(message.buildInterested());
+  if (isHandshake(msg)){
+        socket.write(message.buildInterested());
+  } 
+  else{
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
 }
 
 
@@ -45,3 +56,23 @@ function isHandshake(msg) {
   return msg.length === msg.readUInt8(0) + 49 &&
          msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
+
+function chokeHandler() { 
+    //.....
+ }
+
+function unchokeHandler() { 
+  //...
+ }
+
+function haveHandler(payload) { 
+  //...
+ }
+
+function bitfieldHandler(payload) { 
+  //...
+ }
+
+function pieceHandler(payload) { 
+  //...
+ }
